efed675202b7c3feaaf1645784ba873d8641e87f
UBoiii <der_uboiii@outlook.com>
2024-05-02 15:51:16 +0100

    Set window to not be resizable and added comments to CustomFrame

--- src/mswing/CustomFrame.java
+++ src/mswing/CustomFrame.java
@@ -8,14 +8,16 @@ import javax.swing.JFrame;
 public class CustomFrame extends JFrame{
 
   private void initialize(){
-    
+    // Define basic properties of the window
     setSize(800,550);
     setLocationRelativeTo(null);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     getContentPane().setBackground(new Color(0xFBFBFB));
+    // Setting the icon image
     setIconImage(new ImageIcon("assets/hotel2.png").getImage());
-    // setUndecorated(true);
-
+    
+    // Disabling resizing so that the window keeps the same size no matter what
+    setResizable(false);
     setVisible(true);
   }
 
