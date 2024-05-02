6974dbb817f0653bb164bf5622842b2295a242fe
karraamine <aminekarra606@gmail.com>
2024-05-02 02:21:13 -0700

    updatedDB

--- src/DB/schema.sql
+++ src/DB/schema.sql
@@ -7,24 +7,12 @@ CREATE TABLE Client (
     Phone VARCHAR(20)
 );
 
--- Create Hotel table
-CREATE TABLE Hotel (
-    HotelID INT PRIMARY KEY AUTO_INCREMENT,
-    Name VARCHAR(100),
-    Location VARCHAR(100),
-    Description TEXT
-
-);
-
 -- Create Room table
 CREATE TABLE Room (
     RoomID INT PRIMARY KEY AUTO_INCREMENT,
-    HotelID INT,
-    Type VARCHAR(50),
+    Title VARCHAR(50),
     Price DECIMAL(10, 2),
-    Availability BOOLEAN,
-    Image      BLOB,
-    FOREIGN KEY (HotelID) REFERENCES Hotel(HotelID)
+    Image      BLOB
 );
 
 -- Create Reservation table
@@ -38,15 +26,6 @@ CREATE TABLE Reservation (
     Status VARCHAR(50),
     FOREIGN KEY (ClientID) REFERENCES Client(ClientID),
     FOREIGN KEY (RoomID) REFERENCES Room(RoomID)
-);
-
--- Create Discount table
-CREATE TABLE Discount (
-    DiscountID INT PRIMARY KEY AUTO_INCREMENT,
-    HotelID INT,
-    DiscountPercentage DECIMAL(5, 2),
-    Description TEXT,
-    FOREIGN KEY (HotelID) REFERENCES Hotel(HotelID)
 );
 
 -- Create Admin table
