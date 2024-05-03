7854b4a42bceb089b38cdff3bfe0a76103895cdc
Mouadspace <mouadsaif.ms@gmail.com>
2024-05-03 02:37:20 +0100

    app.java unstaged

--- src/App.java
+++ src/App.java
@@ -1,14 +1,12 @@
 import java.awt.FontFormatException;
 import java.io.IOException;
 
-import ui.Home;
 import ui.Login;
 
 public class App {
     
     public App() throws FontFormatException, IOException {
-        // new  Login();
-        new Home();
+        new  Login();
     }
 
     public static void main(String[] args) throws Exception {
