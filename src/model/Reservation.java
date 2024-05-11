ee4eb9f480190e29b6dc7bf1146d2934e0ef97ee
Oubayhe Ot <oubayhe.ot@gmail.com>
2024-05-11 04:58:31 +0100

    Adding some files concerning the previous commit

---
+++ src/model/Reservation.java
@@ -0,0 +1,187 @@
+package model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import DB.DataBase;
+
+public class Reservation {
+    private int ReservationID;
+    private int RoomID;
+    private double TotalPrice;
+    private LocalDate checkIn;
+    private LocalDate checkOut;
+    private String roomType; 
+    private String imagePath;
+    private int maxMembers;
+    private int bathrooms;
+    private int bedrooms;
+    private String description;
+    
+
+    public int getReservationID() {
+        return ReservationID;
+    }
+
+    public void setReservationID(int reservationID) {
+        ReservationID = reservationID;
+    }
+
+    public int getRoomID() {
+        return RoomID;
+    }
+
+    public void setRoomID(int roomID) {
+        RoomID = roomID;
+    }
+
+    public double getTotalPrice() {
+        return TotalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        TotalPrice = totalPrice;
+    }
+
+    public LocalDate getCheckIn() {
+        return checkIn;
+    }
+
+    public void setCheckIn(LocalDate checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public LocalDate getCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(LocalDate checkOut) {
+        this.checkOut = checkOut;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public int getMaxMembers() {
+        return maxMembers;
+    }
+
+    public void setMaxMembers(int maxMembers) {
+        this.maxMembers = maxMembers;
+    }
+
+    public int getBathrooms() {
+        return bathrooms;
+    }
+
+    public void setBathrooms(int bathrooms) {
+        this.bathrooms = bathrooms;
+    }
+
+    public int getBedrooms() {
+        return bedrooms;
+    }
+
+    public void setBedrooms(int bedrooms) {
+        this.bedrooms = bedrooms;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Reservation(int ReservationID, int RoomID, double TotalPrice, LocalDate checkIn, LocalDate checkOut, String roomType, String imagePath, int maxMembers, int bathrooms, int bedrooms, String description) {
+        this.ReservationID = ReservationID;
+        this.RoomID = RoomID;
+        this.TotalPrice = TotalPrice;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+        this.roomType = roomType;
+        this.imagePath = imagePath;
+        this.maxMembers = maxMembers;
+        this.bathrooms = bathrooms;
+        this.bedrooms = bedrooms;
+        this.description = description;
+    }
+
+    public Reservation() {
+        
+    }
+
+    public Reservation(Reservation reservedRoom) {
+        this.ReservationID = reservedRoom.getReservationID();
+        this.RoomID = reservedRoom.getRoomID();
+        this.TotalPrice = reservedRoom.getTotalPrice();
+        this.checkIn = reservedRoom.getCheckIn();
+        this.checkOut = reservedRoom.getCheckOut();
+        this.roomType = reservedRoom.getRoomType();
+        this.imagePath = reservedRoom.getImagePath();
+        this.maxMembers = reservedRoom.getMaxMembers();
+        this.bathrooms = reservedRoom.getBathrooms();
+        this.bedrooms = reservedRoom.getBedrooms();
+        this.description = reservedRoom.getDescription();
+    }
+
+
+    
+
+    // This is to get all the reservations of a client, through his/her id.
+    // The data that we're selecting is what we need to display in the Reservation page
+    // Either of the user is in the Right panel, or just in card in the main content.
+    static public ArrayList<Reservation> getReservations(int ClientID) throws SQLException {
+        ArrayList<Reservation> result = new ArrayList<Reservation>();
+        String query = "SELECT  rsv.ReservationID, rm.roomID, rsv.TotalPrice, rsv.CheckInDate, \n" + //
+                        "rsv.CheckOutDate, rm.roomType, rm.imagePath, dt.Max_Members, dt.Bathroom,\n" + //
+                        "dt.Bedroom, dt.Description FROM Reservation rsv\n" + //
+                        "JOIN Room rm ON rsv.RoomID = rm.RoomID\n" + //
+                        "JOIN RoomDetails dt ON rm.RoomID = dt.RoomID\n" + //
+                        "WHERE rsv.ClientID = 4" ;
+        ResultSet resultSet = DataBase.getStatement().executeQuery(query);
+        while (resultSet.next())
+        {
+            result.add(
+                new Reservation(
+                    resultSet.getInt(1), 
+                    resultSet.getInt(2),
+                    resultSet.getDouble(3),
+                    resultSet.getDate(4).toLocalDate(),
+                    resultSet.getDate(5).toLocalDate(),
+                    resultSet.getString(6),
+                    resultSet.getString(7),
+                    resultSet.getInt(8),
+                    resultSet.getInt(9),
+                    resultSet.getInt(10),
+                    resultSet.getString(11)
+                ) 
+            );
+        }
+        return result;
+    }
+    
+    // This is to cancel the reservation:
+    public void cancelReservation() throws SQLException {
+        int reservationId = this.ReservationID;
+        String query = "DELETE FROM Reservation WHERE ReservationID = " + reservationId;
+        // Assuming you already have a connection object
+        int rowsAffected = DataBase.getStatement().executeUpdate(query);
+    }
+}
